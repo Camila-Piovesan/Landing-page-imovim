3eeb139bdcb62638bc80cd73aacbd042c5bf53da
Camila-Piovesan <camilapiovesan89@gmail.com>
2023-04-17 23:10:13 -0300

    add devs info

--- src/pages/PaginaBaixar/index.jsx
+++ src/pages/PaginaBaixar/index.jsx
@@ -4,6 +4,9 @@ import {Link} from 'react-router-dom';
 import logo from '../../assets/logo.svg';
 import marcador from '../../assets/marcador.svg'
 import qrcode from '../../assets/qrcodeAleatorio.svg';
+import iconPessoas from '../../assets/pessoas.svg';
+import iconEventos from '../../assets/evento.svg';
+import iconChat from '../../assets/chat.svg';
 
 function index() {
     return (
@@ -32,6 +35,59 @@ function index() {
         <text id="qrcodeText">Ou escaneie o QR Code abaixo</text>
 
         <img id="qrCode" src={qrcode}/>
+
+
+        <container>
+            <div id="fundoGradiente">
+                <img className="fotoDevs" src={require('../../assets/fotoCamila.png')} />
+                <img className="fotoDevs" src={require('../../assets/fotoTiago.png')} />
+                <img className="fotoDevs" src={require('../../assets/fotoLuis.png')} />
+                <img className="fotoDevs" src={require('../../assets/fotoOsmar.png')} />
+            </div>
+ 
+            <div id="textFundoGradiente">
+
+                <text id="apresentaçãoDevs">Conheça mais dos desenvolvedores!</text>
+                <text className="textDevs">
+                    <text id="nomeDevs"> Camila Piovesan </text>
+                    <br/> Design 
+                    <br/> Documentação
+                </text>
+
+                <text className="textDevs">
+                    <text id="nomeDevs"> Tiago Alves </text>
+                    <br/> Front End
+                </text>
+
+                <text className="textDevs">
+                    <text id="nomeDevs"> Luís Felipe </text>
+                    <br/> Back End
+                    <br/> Banco de Dados
+                </text>
+
+                <text className="textDevs">
+                    <text id="nomeDevs"> Osmar Bruno</text>
+                    <br/> Fullstack
+                </text>
+            </div>
+        </container>
+
+        <container className="boxContainer">
+            <div className="box">
+                <img className="icon" src={iconPessoas}/> <br/>
+                <text id="textBox">Conheça pessoas <br/> novas</text>
+            </div>
+
+            <div className="box">
+                <img className="icon" src={iconEventos}/> <br/>
+                <text id="textBox">Marque eventos de seus esportes</text>
+            </div>
+
+            <div className="box">
+                <img className="icon" src={iconChat}/> <br/>
+                <text id="textBox">Converse com seus amigos</text>
+            </div>
+        </container>
         </>
 
     )
